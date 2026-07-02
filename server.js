269417f1d10e79964c19f2d01327a76fc4dfd12e
Rahul <rahulbhardwaz2k1@gmail.com>
2026-07-02 23:46:35 +0530

    Allow Vercel domains in CORS

--- server.js
+++ server.js
@@ -39,15 +39,28 @@ app.use(helmet());
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  process.env.FRONTEND_URL // Dynamic production URL
+  process.env.FRONTEND_URL, // Dynamic production URL
+  'https://jewels-and-you-sne7.vercel.app',
+  'https://jewels-and-you.vercel.app'
 ].filter(Boolean);
 
-app.use(cors({
-  origin: allowedOrigins,
+// Dynamic origin check to allow any Vercel preview URLs
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
+};
+
+app.use(cors(corsOptions));
 
 // Add CORS headers for static files
 app.use('/uploads', (req, res, next) => {
